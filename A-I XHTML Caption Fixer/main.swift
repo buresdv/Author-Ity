4f0f636b61944aa8232166aa139d77d7383c55ee
David Bureš <buresdv@gmail.com>
2022-04-23 19:38:29 +0200

    Made check for arguments more performant

--- A-I XHTML Caption Fixer/main.swift
+++ A-I XHTML Caption Fixer/main.swift
@@ -23,36 +23,34 @@ fixImageCaptions [cesta ke složce XHTML] [cesta ke složce XML]
     let xhtmlFolder: String = arguments[0]
     let xmlFolder: String = arguments[1]
     
-    // MARK: XML
     if !existsAtPath(atPath: xmlFolder) { // XML has to be handled first because it contains all the captions
         writeToConsole(message: "Neplatná cesta ke složce XML", format: .error)
         exit(0)
+    }
+    
+    if !existsAtPath(atPath: xhtmlFolder) {
+        writeToConsole(message: "Neplatná cesta ke složce XHTML", format: .error)
+        exit(0)
         
-    } else {
-        
-        // MARK: Load XML
-        let loadedXMLFile: String = findFilesInFolder(inFolder: xmlFolder, desiredExtension: ".xml").first ?? "Soubor to našlo, ale nepovedlo se ho převést na cestu. wtf jak se to stalo" // Get the name of the XML file from the array
-        let loadedXMLFilePath: String = "\(xmlFolder)/\(loadedXMLFile)"
-        
-        let XMLContents: String = loadContentsOfFileIntoString(atPath: loadedXMLFilePath)
-        
-        let extractedIDsAndCaptions = parseXML(rawXML: XMLContents)
-        
-        // MARK: XHTML
-        if !existsAtPath(atPath: xhtmlFolder) {
-            writeToConsole(message: "Neplatná cesta ke složce XHTML", format: .error)
-            exit(0)
-            
-        } else {
-            
-            // MARK: Load XHTML
-            let loadedXHTMLFiles: [String] = findFilesInFolder(inFolder: xhtmlFolder, desiredExtension: ".htm")
-            
-            print(loadedXHTMLFiles)
-            
-            for dictionaryItem in extractedIDsAndCaptions {
-                searchFilesForImageIDs(forID: dictionaryItem.key, atPath: xhtmlFolder, inFiles: loadedXHTMLFiles)
-            }
-        }
+    }
+    
+    // MARK: XML
+    // MARK: Load XML
+    let loadedXMLFile: String = findFilesInFolder(inFolder: xmlFolder, desiredExtension: ".xml").first ?? "Soubor to našlo, ale nepovedlo se ho převést na cestu. wtf jak se to stalo" // Get the name of the XML file from the array
+    let loadedXMLFilePath: String = "\(xmlFolder)/\(loadedXMLFile)"
+    
+    let XMLContents: String = loadContentsOfFileIntoString(atPath: loadedXMLFilePath)
+    
+    let extractedIDsAndCaptions = parseXML(rawXML: XMLContents)
+    
+    
+    // MARK: XHTML
+    // MARK: Load XHTML
+    let loadedXHTMLFiles: [String] = findFilesInFolder(inFolder: xhtmlFolder, desiredExtension: ".htm")
+    
+    print(loadedXHTMLFiles)
+    
+    for dictionaryItem in extractedIDsAndCaptions {
+        searchFilesForImageIDs(forID: dictionaryItem.key, atPath: xhtmlFolder, inFiles: loadedXHTMLFiles)
     }
 }
